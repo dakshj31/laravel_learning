7796f1945edf0fb3b423f46708fe6cf0b6fdb24c
dakshj31 <dakshjoshi1355@gmail.com>
2025-02-10 23:10:48 +0530

    5

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Job;
+use Illuminate\Http\RedirectResponse;
 
 class JobController extends Controller
 {
@@ -12,13 +14,8 @@ class JobController extends Controller
      */
     public function index(): view
     {
-        $jobs = [
-            'Web developer',
-            'Database Admin',
-            'Software Engineer',
-            'Systems Analyst',
-        ];
-        return view('jobs.index', compact('jobs'));
+        $jobs = Job::all();
+        return view('jobs.index')->with('jobs', $jobs);
     }
 
     /**
@@ -32,17 +29,27 @@ class JobController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): string
+    public function store(Request $request): RedirectResponse
     {
-        return 'store';
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        Job::create([
+            'title' => $validatedData['title'],
+            'description' => $validatedData['description']
+        ]);
+
+        return redirect()->route('jobs.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id): string
+    public function show(Job $job): View
     {
-        return 'show';
+        return view('jobs.show')->with('job', $job);
     }
 
     /**
